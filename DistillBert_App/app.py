197b6e2fcfeb5350afb41b0bff2588db02466c5f
mayanksinghkgp <90528116+mayanksinghkgp@users.noreply.github.com>
2021-12-21 21:41:10 +0530

    Update app.py

--- DistillBert_App/app.py
+++ DistillBert_App/app.py
@@ -22,4 +22,4 @@ def get_sentiment():
 
     
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 5000, debug = True, use_reloader = False)
+    app.run(host = '0.0.0.0', port = 5000, debug = True, use_reloader = False) #default port = 5000
